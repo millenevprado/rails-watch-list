8e7f1c6aae9dbe1c72f426a97c1de7bafa67fb3e
millene prado <millene_vprado@hotmail.com>
2021-08-19 15:40:02 -0300

    routes, controller & list views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # list all lists
+  # get 'lists', to: 'lists#index'
+
+  # show a form to create a new list
+  # get 'lists/new', to: 'lists#new'
+
+  # show a list
+  # get 'lists/:id', to: 'lists#show', as: :list
+
+  # save list into db
+  # post 'lists', to: 'lists#create'
+  resources :lists, except: %i[edit update destroy] do
+    resources :bookmarks, only: %i[new create destroy]
+  end
+
+
 end
