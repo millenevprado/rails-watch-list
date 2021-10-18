395b9126e8193a22476a843cb8e8eb0c0dd5e4fe
millene prado <millene_vprado@hotmail.com>
2021-10-17 22:03:44 -0300

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,20 @@ Movie.destroy_all
 
 puts 'Creating movies...'
 
-response = URI.open('http://tmdb.lewagon.com/movie/top_rated')
-json = JSON.parse(response.read)
-results = json['results']
-
-# recebo um array de hashes
-# cada hash tem title, overview, vote_avarege, poster_path
-# quero esses dados para construir meus movies usando meu modelo
-image_base_url = 'https://image.tmdb.org/t/p/w200'
-
-results[0..20].each do |movie_info|
-  movie = Movie.new(
-    title: movie_info['title'],
-    overview: movie_info['overview'],
-    rating: movie_info['vote_average'],
-    poster_url: "#{image_base_url}#{movie_info['poster_path']}"
-  )
-
-  movie.save!
-  puts "Created #{movie.title}"
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+10.times do |i|
+  puts "Importing movies from page #{i + 1}"
+  movies = JSON.parse(open("#{url}?page=#{i + 1}").read)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    base_poster_url = 'https://image.tmdb.org/t/p/original'
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+      rating: movie['vote_average']
+    )
+  end
 end
 
 puts 'Finished!'
